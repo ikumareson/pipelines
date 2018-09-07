b8cd8112249119c897e7b50ec59b3bb7d6e20008
Inder <ikumareson@hcl.com>
2018-09-07 10:20:35 +0100

    WIP

--- tasks/deploy-app/task.sh
+++ tasks/deploy-app/task.sh
@@ -2,7 +2,9 @@
 
 set -e 
 
-pushd spring-music-app
+tar xzvf build-store/*.tgz
+
+pushd spring-music
 cf api $CF_ENDPOINT --skip-ssl-validation
 cf login -u $CF_USER -p $CF_PASSWORD -o $CF_ORG -s $CF_SPACE
 cf apps
