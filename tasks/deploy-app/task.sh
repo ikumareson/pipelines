6b7dbad440c0c321e5681c244fefe24ff2a35414
Inder <ikumareson@hcl.com>
2018-09-09 12:54:34 +0100

    WIP

--- tasks/deploy-app/task.sh
+++ tasks/deploy-app/task.sh
@@ -6,7 +6,6 @@ pushd spring-music
 
 cf api $CF_ENDPOINT --skip-ssl-validation
 cf login -u $CF_USER -p $CF_PASSWORD -o $CF_ORG -s $CF_SPACE
-cf apps
 
 cf check-route spring-music apps.nonprod.dryice01.in.hclcnlabs.com| grep "does not exist"
 if [ $? -eq 0 ]; then
@@ -26,22 +25,22 @@ BLUE_APP="$(cf a | grep "^spring-music" | awk '{print $1}')"
 
 if [ "$(echo "$BLUE_APP" | wc -l)" -gt 1 ]; then
 	echo "Multiple versions of app exist.. Aborting Deploy."
+	cf apps
 	exit 1
 fi
 
-if [ "$BLUE_APP" -eq "" ]; then
+if [ "$BLUE_APP" == "" ]; then
 	echo "No version of app exists.. Running New Deploy."
 
-	cf push $GREEN_APP --no-route
-	cf map-route $GREEN_APP apps.nonprod.dryice01.in.hclcnlabs.com --hostname spring-music
+	timestamp="$(date +'%Y%m%d%H%M%S')"
+	cf push spring-music_$timestamp --no-route
+	cf map-route spring-music_$timestamp apps.nonprod.dryice01.in.hclcnlabs.com --hostname spring-music
 
 	cf apps
 else
 	timestamp="$(date +'%Y%m%d%H%M%S')"
-	GREEN_APP="spring_music_$timestamp"
-
+	GREEN_APP="spring-music_$timestamp"
 	echo "Found 'blue' app $BLUE_APP, deploying 'green' app $GREEN_APP"
-
 	cf push $GREEN_APP --no-route
 	cf map-route $GREEN_APP apps.nonprod.dryice01.in.hclcnlabs.com --hostname spring-music-temp
 fi
